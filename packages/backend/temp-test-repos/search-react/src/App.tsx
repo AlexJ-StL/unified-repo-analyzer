1054d067e32c62affa1180275958edf85bd68c1c
Alex Jensen <thanotosq@gmail.om>
2025-08-28 18:57:50 -0500

    Add React TypeScript test project in search-react directory
    Signed-off-by: Alex Jensen <thanotosq@gmail.om>

---
+++ packages/backend/temp-test-repos/search-react/src/App.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+interface Props {
+  title: string;
+}
+
+const App: React.FC<Props> = ({ title }) => {
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+      <p>Welcome to the test React application</p>
+    </div>
+  );
+};
+
+export default App;
