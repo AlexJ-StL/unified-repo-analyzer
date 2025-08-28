44cbc9b7d1125948663bc97f3a6b783e1185d2f4
Alex Jensen <thanotosq@gmail.om>
2025-08-28 18:36:50 -0500

    Add test repos for concurrent processing and export functionality
    Create multiple test repositories for concurrent processing (concurrent-4 through concurrent-9) with Express apps, add batch-js implementation, and create test directories for export functionality. Update yarn.lock to reflect new dependencies.
    
    Signed-off-by: Alex Jensen <thanotosq@gmail.om>

---
+++ packages/backend/temp-test-repos/concurrent-9/app-9.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const _ = require('lodash');
+
+const app9 = express();
+
+app9.get('/', (req, res) => {
+  res.json({
+    message: 'Hello from app 9',
+    data: _.range(1, 100)
+  });
+});
+
+app9.listen(3009, () => {
+  console.log('App 9 running on port 3009');
+});
+
+module.exports = app9;
