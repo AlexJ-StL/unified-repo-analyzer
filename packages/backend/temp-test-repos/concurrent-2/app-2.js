4f56bc187997bf94d95ad6efefab2b8bc68beea7
Alex Jensen <thanotosq@gmail.om>
2025-08-28 18:35:17 -0500

    Add test repositories for batch processing, concurrent operations, and multiple languages
    Signed-off-by: Alex Jensen <thanotosq@gmail.om>

---
+++ packages/backend/temp-test-repos/concurrent-2/app-2.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const _ = require('lodash');
+
+const app2 = express();
+
+app2.get('/', (req, res) => {
+  res.json({
+    message: 'Hello from app 2',
+    data: _.range(1, 100)
+  });
+});
+
+app2.listen(3002, () => {
+  console.log('App 2 running on port 3002');
+});
+
+module.exports = app2;
