46acf03f9d955ec3ffb96bdfded87e64719993a3
Alex Jensen <thanotosq@gmail.om>
2025-08-29 14:26:19 -0500

    Fix stdout maxBuffer error in getWorkingState function
    Add error handling for 'stdout maxBuffer length exceeded' error
    when calling getWorkingState. Return empty object instead of
    crashing when buffer limit is exceeded.
    
    Signed-off-by: Alex Jensen <thanotosq@gmail.om>

--- packages/backend/temp-test-repos/concurrent-0/app-0.js
+++ packages/backend/temp-test-repos/concurrent-0/app-0.js
@@ -1,13 +1,12 @@
-
 const express = require('express');
 const _ = require('lodash');
 
 const app0 = express();
 
-app0.get('/', (req, res) => {
+app0.get('/', (_req, res) => {
   res.json({
     message: 'Hello from app 0',
-    data: _.range(1, 100)
+    data: _.range(1, 100),
   });
 });
 
--- packages/backend/temp-test-repos/concurrent-5/app-5.js
+++ packages/backend/temp-test-repos/concurrent-5/app-5.js
@@ -1,13 +1,12 @@
-
 const express = require('express');
 const _ = require('lodash');
 
 const app5 = express();
 
-app5.get('/', (req, res) => {
+app5.get('/', (_req, res) => {
   res.json({
     message: 'Hello from app 5',
-    data: _.range(1, 100)
+    data: _.range(1, 100),
   });
 });
 
