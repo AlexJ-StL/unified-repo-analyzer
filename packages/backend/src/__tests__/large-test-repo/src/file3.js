dbe33c870e88bc6b9b28e9650b5bc2a2b1ac5201
Alex Jensen <thanotosq@gmail.om>
2025-08-29 02:34:12 -0500

    Increase maxBuffer size to prevent stdout length exceeded error
    Increase maxBuffer size from 1024000 to 1048576 bytes in getWorkingState function to prevent 'stdout maxBuffer length exceeded' error when capturing command outputs.
    
    Signed-off-by: Alex Jensen <thanotosq@gmail.om>

--- packages/backend/src/__tests__/large-test-repo/src/file3.js
+++ packages/backend/src/__tests__/large-test-repo/src/file3.js
@@ -1,120 +1,118 @@
 // file3.js in src
 
-
-function function0() {
+function _function0() {
   const data = {
     id: 0,
     name: 'Function 0',
     timestamp: Date.now(),
   };
-  
+
   return data;
 }
-        
-function function1() {
+
+function _function1() {
   const data = {
     id: 1,
     name: 'Function 1',
     timestamp: Date.now(),
   };
-  
+
   return data;
 }
-        
-function function2() {
+
+function _function2() {
   const data = {
     id: 2,
     name: 'Function 2',
     timestamp: Date.now(),
   };
-  
+
   return data;
 }
-        
-function function3() {
+
+function _function3() {
   const data = {
     id: 3,
     name: 'Function 3',
     timestamp: Date.now(),
   };
-  
+
   return data;
 }
-        
-function function4() {
+
+function _function4() {
   const data = {
     id: 4,
     name: 'Function 4',
     timestamp: Date.now(),
   };
-  
+
   return data;
 }
-        
-function function5() {
+
+function _function5() {
   const data = {
     id: 5,
     name: 'Function 5',
     timestamp: Date.now(),
   };
-  
+
   return data;
 }
-        
-function function6() {
+
+function _function6() {
   const data = {
     id: 6,
     name: 'Function 6',
     timestamp: Date.now(),
   };
-  
+
   return data;
 }
-        
-function function7() {
+
+function _function7() {
   const data = {
     id: 7,
     name: 'Function 7',
     timestamp: Date.now(),
   };
-  
+
   return data;
 }
-        
-function function8() {
+
+function _function8() {
   const data = {
     id: 8,
     name: 'Function 8',
     timestamp: Date.now(),
   };
-  
+
   return data;
 }
-        
-function function9() {
+
+function _function9() {
   const data = {
     id: 9,
     name: 'Function 9',
     timestamp: Date.now(),
   };
-  
+
   return data;
 }
-        
+
 class Class3 {
   constructor(name) {
     this.name = name;
     this.id = Math.random().toString(36);
   }
-  
+
   getName() {
     return this.name;
   }
-  
+
   getId() {
     return this.id;
   }
 }
 
 module.exports = { Class3 };
-      
